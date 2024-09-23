6acf8340131a6d75ca2b6188cb7c25af69ab0c23
woogi <azicplus@naver.com>
2024-09-23 19:37:26 +0900

    add TopBar

---
+++ DesignSystem/Sources/DesignSystem/CommonUI/TopBar.swift
@@ -0,0 +1,83 @@
+import SwiftUI
+
+struct TopBarModifier<Left, Right>: ViewModifier where Left: View, Right: View {
+    var title: String
+    var titleFont: Font
+    var titleColor: Color
+    var backgroundColor: Color
+    var padding: (Edge.Set, CGFloat?)
+    var leftItem: (() -> Left)?
+    var rightItem: (() -> Right)?
+    
+    func body(content: Content) -> some View {
+        VStack(spacing: 0) {
+            TopBar(
+                title: self.title,
+                titleFont: self.titleFont,
+                titleColor: self.titleColor,
+                backgroundColor: self.backgroundColor,
+                padding: self.padding,
+                leftItem: self.leftItem,
+                rightItem: self.rightItem
+            )
+            .zIndex(999)
+            content
+                .frame(maxHeight: .infinity, alignment: .top)
+        }
+        .navigationBarHidden(true)
+    }
+}
+
+struct TopBar<Left, Right>: View where Left: View, Right: View {
+    var title: String
+    var titleFont: Font
+    var titleColor: Color
+    var backgroundColor: Color
+    var padding: (Edge.Set, CGFloat?)
+    var leftItem: (() -> Left)?
+    var rightItem: (() -> Right)?
+    
+    var body: some View {
+        HStack {
+            self.leftItem?()
+            Spacer()
+            self.rightItem?()
+        }
+        .overlay(
+            Text(self.title)
+                .font(titleFont)
+                .foregroundColor(self.titleColor)
+                .frame(minWidth: 140, minHeight: 44)
+        )
+        .padding(padding.0, padding.1)
+        .frame(height: 44)
+        .background(
+            self.backgroundColor
+                .ignoresSafeArea()
+        )
+    }
+}
+
+extension View {
+    func topBar<Left: View, Right: View>(
+        title: String,
+        titleFont: Font = .spoqaMedium(fontSize: 18),
+        titleColor: Color = .gray900,
+        backgroundColor: Color = .clear,
+        padding: (Edge.Set, CGFloat?) = (.horizontal, 18),
+        @ViewBuilder leftItem: @escaping () -> Left,
+        @ViewBuilder rightItem: @escaping () -> Right
+    ) -> some View {
+        modifier(
+            TopBarModifier(
+                title: title,
+                titleFont: titleFont,
+                titleColor: titleColor,
+                backgroundColor: backgroundColor,
+                padding: padding,
+                leftItem: leftItem,
+                rightItem: rightItem
+            )
+        )
+    }
+}
